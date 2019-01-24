6d12fdf1ffa74d50c7560134be025658babec9ed
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-01-24 16:51:26 -0500

    did some testing

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,15 @@ class Person {
         return `Hello my name is ${this.name}. I am from ${this.location}`
     }
 }
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  fred(speak);
