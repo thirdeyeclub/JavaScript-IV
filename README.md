9460f06ab71147204bcf920230e84b62d410eeb0
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-01-24 17:23:58 -0500

    student tested: test Passed!

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ const fred = new Person({
 * **Fork** and clone this repository. x
 * **Complete** all of the exercises found in the assignment files. x
 
-## `prototype-refactor` - Take existing code and make it modern.
+## `prototype-refactor` - Take existing code and make it modern.x
 
 * You're going to work with your prototypes assignment you built out yesterday.
 * `Challenge:` **Convert** all of your constructors into ES6 Classes using the `class` and `extends` keywords. You should be able to run your same logs and they should build out the proper expected behaviors.
--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -31,3 +31,49 @@ const neeko = new Person({
     grade(student, subject) {
         return `${student.name} receives a perfect score on ${subject}`;}
   }
+
+  //test instructer!!
+const yogurt = new Instructor({
+    name: 'Yogurt',
+    location: 'Sand Palace',
+    age: 420,
+    gender: 'male',
+    specialty: 'the swarts',
+    favLangauge: 'sandscript',
+    catchPrase: 'USE THE SWARTS!'
+  });
+
+  class Student extends Person{
+      constructor(trait){
+          super(trait);
+          this.previousBackground = trait.previousBackground;
+          this.className = trait.className;
+          this.favSubject = trait.favSubject; 
+      }
+    listsSubjects() {
+    return (this.favSubjects.map((subject) => {
+            console.log(subject);
+    }))
+    }
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+    graduate () {
+        return `${this.name} has gained sufficient credits to graduate from Lambda School! We wish them all the best in paying off their ISAs!`;
+}
+     }
+
+//test Student
+
+const yeri = new Student({
+    name: 'Yeri', 
+    age : 18, 
+    location: 'Indoors', 
+    gender: 'M',
+    previousBackground: 'Speznaught',
+    className: 'WEB17',
+    favSubjects: 'Place And Control'
+});
